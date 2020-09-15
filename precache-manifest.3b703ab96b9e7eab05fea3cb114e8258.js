9c242983c34b2025e691a1a6b44821201d49b177
Nicholas Trunov <anteycu@gmail.com>
2020-09-15 10:11:42 +0300

    Updates

---
+++ precache-manifest.3b703ab96b9e7eab05fea3cb114e8258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9edf7a636a7eab6bf179ed28fa74db",
+    "url": "/goit-react-hw-03-phonebook/index.html"
+  },
+  {
+    "revision": "29365b6cb483fb83b2be",
+    "url": "/goit-react-hw-03-phonebook/static/css/main.22f970ab.chunk.css"
+  },
+  {
+    "revision": "6618b86b98e6250d937a",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.a084f957.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-03-phonebook/static/js/2.a084f957.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29365b6cb483fb83b2be",
+    "url": "/goit-react-hw-03-phonebook/static/js/main.02a61042.chunk.js"
+  },
+  {
+    "revision": "2666e4603425ed45866b",
+    "url": "/goit-react-hw-03-phonebook/static/js/runtime-main.2b56cb65.js"
+  }
+]);
